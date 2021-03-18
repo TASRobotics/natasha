ea20212986415975250937e8f156665dff23b045
balta-z-r <zunigaruizb@gmail.com>
2021-03-18 12:57:08 +0800

    feat: pixi graphics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect, useReducer, useRef } from 'react';
 import { io } from 'socket.io-client';
+import { Stage, Sprite, useTick, Container } from '@inlet/react-pixi';
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+type Action = {
+  type: string;
+  data: Point;
+};
 
 const App = () => {
   const [messages, setMessages] = useState<string[]>([]);
@@ -21,14 +31,58 @@ const App = () => {
     };
   }, []);
 
+  const reducer = (prevPoint: Point, { data }: Action) => {
+    return {
+      x: prevPoint.x + data.x,
+      y: prevPoint.y + data.y
+    };
+  };
+  const Bunny = () => {
+    const [motion, update] = useReducer(reducer, {
+      x: 0,
+      y: 0
+    });
+
+    useEffect(() => {
+      document.addEventListener('keydown', (e: KeyboardEvent) => {
+        let data = { x: 0, y: 0 };
+        switch (e.keyCode) {
+          case 87:
+            data.y = -1;
+            break;
+          case 83:
+            data.y = 1;
+            break;
+          case 68:
+            data.x = 1;
+            break;
+          case 65:
+            data.x = -1;
+            break;
+          default:
+            break;
+        }
+        update({
+          type: 'update',
+          data
+        });
+      });
+    }, []);
+
+    return (
+      <Sprite
+        image='https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png'
+        {...motion}
+      />
+    );
+  };
+
   return (
-    <div>
-      <ul>
-        {messages.map((message) => (
-          <li>{message}</li>
-        ))}
-      </ul>
-    </div>
+    <Stage width={300} height={300} options={{ transparent: true }}>
+      <Container x={150} y={150}>
+        <Bunny />
+      </Container>
+    </Stage>
   );
 };
 
